23d84526bae92fd5e359854475b368cff9a3736e
Alberto Minoru <alberto.minoru@gmail.com>
2020-08-19 20:06:12 -0300

    Arquivos referentes das aulas 6 e 7

---
+++ admin/Views.py
@@ -0,0 +1,49 @@
+from flask_admin import AdminIndexView, expose
+from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
+from flask import redirect
+from config import app_config, app_active
+from model import *
+
+config = app_config[app_active]
+
+
+class HomeView(AdminIndexView):
+    extra_css = ['https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css']
+    extra_js = ['https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js', '/static/js/chart.js']
+
+    @expose('/')
+    def index(self):
+        return self.render('home.html')
+
+    def is_accessible(self):
+        return True
+
+    # def inaccessible_callback(self, name, **kwargs):
+    #     if current_user.is_authenticated:
+    #         return redirect('/admin')
+    #     else:
+    #         return redirect('/login')
+
+
+class UserView(ModelView):
+    def on_model_change(self, form, User, is_created):
+        if 'password' in form:
+            if form.password.data is not None:
+                User.set_password(form.password.data)
+            else:
+                del form.password
+
+    def is_accessible(self):
+        return True
+
+
+class GenericView(ModelView):
+    def is_accessible(self):
+        return True
+
+    # def inaccessible_callback(self,name,**kwargs):
+    #     if current_user.is_authenticated:
+    #         return redirect('/admin')
+    #     else:
+    #         return redirect('/login')
--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from admin.Admin import start_views
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(config):
@@ -10,13 +13,20 @@ def create_app(config):
     app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    app.config['FLASK_ADMIN_SWATCH'] = 'paper'
+    db = SQLAlchemy(app)
+
+    start_views(app, db)
+    Bootstrap(app)
+
+    db.init_app(app)
     config.APP = app
 
     @app.after_request
     def after_request(response):
-        response.header.add('Access-Control-Allow-Origin', '*')
-        response.header.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.header.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+        # response.header.add('Access-Control-Allow-Origin', '*')
+        # response.header.add('Access-Control-Allow-Headers', 'Content-Type')
+        # response.header.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
         return response
 
     @app.route('/')
